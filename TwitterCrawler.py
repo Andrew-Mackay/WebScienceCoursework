d955bcf25dad8fe9a4bcaceb39837fde022264c8
Andrew Mackay <mackay-andrew@hotmail.com>
2018-10-09 20:40:18 +0100

    Basic streaming working

--- TwitterCrawler.py
+++ TwitterCrawler.py
@@ -5,7 +5,18 @@ import tweepy
 client = MongoClient()
 db = client.twitterdb
 
+
+class Listener(tweepy.StreamListener):
+    def on_data(self, raw_data):
+        print(raw_data)
+        return True
+
+    def on_error(self, status_code):
+        print(status_code)
+
+
 auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
 auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
 
-api = tweepy.API(auth)
+twitterStream = tweepy.Stream(auth, Listener())
+twitterStream.filter(track=["car"])
