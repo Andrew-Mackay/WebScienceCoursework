c4de13f3aae56d6889ed36ec4bd80ca945cad692
Andrew Mackay <2192793m@student.gla.ac.uk>
2018-11-01 17:23:31 +0000

    threading and geo check done

--- 1c.py
+++ 1c.py
@@ -8,11 +8,11 @@ import tweepy
 import json
 import time
 import queue
+import _thread
 
 RUN_TIME = 30  # how long program should run for (in minutes)
 GLASGOW_WOEID = 21125  # WOEID for Glasgow
 GEOBOX_GLASGOW = [-4.359484911, 55.8030299038, -4.1260254383, 55.9101684715]  # taken from https://boundingbox.klokantech.com/
-MAX_TWEETS_PER_TREND = 40000  # Only take this many tweets per trend (helps to ensure a balance of tweets)
 COLLECTION_NAME = "geo_tagged_1c"
 
 users_with_geo_tagged_tweets = queue.Queue()
@@ -36,24 +36,34 @@ class Listener(tweepy.StreamListener):
         # returning non-False reconnects the stream, with backoff.
 
 
+def process_user(user_id):
+    for status in tweepy.Cursor(api.user_timeline, id=user_id).items():
+        if status.geo is not None:
+            db[COLLECTION_NAME].insert(status._json)
+    # maybe if 10 not geo in a row break?
+
+def user_based_probes(threadName):
+    print(threadName + " Started.")
+    while True:
+        if not users_with_geo_tagged_tweets.empty():
+            process_user(users_with_geo_tagged_tweets.get())
+
 def sorted_helper(tweet_volume):
     # if no tweet_volume information is available set to 0
     if tweet_volume is None:
         return 0
     return tweet_volume
 
-
-# Returns False if time has expired, True otherwise
-def process_user(api, collection, end_time, user_id):
-    for status in tweepy.Cursor(api.user_timeline, id=user_id).items():
-        if status.geo_enabled == True:
-            db[collection].insert(status._json)
-
-        # Quit if run for over 1 hour
-        if time.time() > end_time:
-            return False
-    return True
-    # maybe if 10 not geo in a row break?
+def trend_based_probes(threadName):
+    print(threadName + " Started.")
+    glasgow_trends = api.trends_place(GLASGOW_WOEID)  # get trends in Glasgow
+    glasgow_trends = glasgow_trends[0]['trends']  # Extract information
+    sorted_glasgow_trends = sorted(glasgow_trends, key=lambda k: sorted_helper(k['tweet_volume']), reverse=True)  # sort trends by tweet volume
+    for trend in sorted_glasgow_trends:
+        print(trend["name"], trend["tweet_volume"])
+        for status in tweepy.Cursor(api.search, q=trend["name"], rpp=100, lang="en").items():
+            if status.geo is not None:
+                db[COLLECTION_NAME].insert(status._json)
 
 auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
 auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
@@ -65,34 +75,25 @@ twitterStream.filter(languages=["en"], async=True, locations=GEOBOX_GLASGOW)
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
+# User based probes and keyword based probes handled on seperate threads
+try:
+   user_thread = _thread.start_new_thread(user_based_probes, ("User_thread",))
+   trend_thread = _thread.start_new_thread(trend_based_probes, ("Trend-thread",))
+except:
+   print("Error: unable to start thread")
+
 while True:
-    if not users_with_geo_tagged_tweets.empty():
-        if not process_user(api, COLLECTION_NAME, time_end, users_with_geo_tagged_tweets.get()):
-            twitterStream.disconnect()
-            exit()
-        
-    # Quit if run for over time period hour
-    if time.time() > end_time:
-        twitterStream.disconnect()
-        exit()
+    # Quit if run for over 1 hour
+    if time.time() < time_end:
+        time.sleep(30)
+    else:
+        break
 
-glasgow_trends = api.trends_place(GLASGOW_WOEID)  # get trends in Glasgow
-glasgow_trends = glasgow_trends[0]['trends']  # Extract information
-sorted_glasgow_trends = sorted(glasgow_trends, key=lambda k: sorted_helper(k['tweet_volume']), reverse=True)  # sort trends by tweet volume
-# print(uk_trends)
-for trend in sorted_glasgow_trends:
-    tweets_collected = 0
-    print(trend["name"], trend["tweet_volume"])
+# Time expired, kill all threads
+twitterStream.disconnect()
+user_thread.exit()
+trend_thread.exit()
+exit()
 
-    for status in tweepy.Cursor(api.search, q=trend["name"], rpp=100, lang="en").items():
-        tweets_collected += 1
-        if status.geo_enabled == True:
-            db[COLLECTION_NAME].insert(status._json)
-        # Move to next trend when collected enough from this trend
-        if tweets_collected > MAX_TWEETS_PER_TREND:
-            break
-        
-        # Quit if run for over 1 hour
-        if time.time() > time_end:
-            twitterStream.disconnect()
-            exit()
+
+
