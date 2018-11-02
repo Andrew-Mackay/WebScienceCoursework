1fc012936ef632245b96a7afd7046f04aa1eded0
Andrew Mackay <2192793m@student.gla.ac.uk>
2018-11-02 19:28:30 +0000

    histogram now groups by minutes

--- histogram.py
+++ histogram.py
@@ -1,11 +1,27 @@
 import matplotlib.pyplot as plt
 from pymongo import MongoClient
+from datetime import datetime
+
 
 def plot_histogram(start_time, end_time, collection_name, title):
     client = MongoClient()
     db = client.twitterdb
     collection = db[collection_name]
+    # within_time_slot = collection.find({
+    #     "created_at": {
+    #         "$gte": start_time,
+    #         "lt": end_time
+    #     }
+    # })
     x = collection.aggregate([
+        {
+            "$match": {
+                "created_at": {
+                    "$gte": start_time,
+                    "$lt": end_time
+                }
+            }
+        },
         {
             '$group': {
                 "_id": { 
@@ -22,5 +38,6 @@ def plot_histogram(start_time, end_time, collection_name, title):
     for y in x:
         print(y)
 
-
-plot_histogram(1,2,"basic_crawler_1a", "foo")
+start = datetime(2018, 11, 2, 19, 0)
+end = datetime(2018, 11, 2, 19, 30)
+plot_histogram(start,end,"geo_tagged_1c", "foo")
