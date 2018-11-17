892dd61e4dd9c2ba4638991aead2c92c817c58b4
Andrew Mackay <2192793m@student.gla.ac.uk>
2018-11-17 17:09:34 +0000

    Part 4 complete

--- 4_graphs.py
+++ 4_graphs.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from datetime import timedelta
 import os
 import numpy as np
+import pandas as pd
+
 
 collection = "flickr_crawler_4_v2"
 client = MongoClient()
@@ -54,4 +56,24 @@ plt.title("Uploads to Flickr by Year")
 plt.legend()
 plt.tight_layout()
 plt.savefig(os.getcwd() + "/year_flickr.svg", format='svg', dpi=1200)
+plt.show()
+
+def to_date(row):
+    return row[0].generation_time
+    
+df = pd.DataFrame(list(db[collection].find({}, {"_id":1})))
+df["created_date"] = df.apply (lambda row: to_date (row),axis=1)
+agg_10m = df.groupby(pd.Grouper(key="created_date", freq='10Min')).size()
+photos_per_10m = agg_10m.values.tolist()[:-1]
+x_labels = range(0, 70, 10)
+
+plt.figure()
+plt.bar(range(len(photos_per_10m)), photos_per_10m, align='edge')
+plt.xticks(range(len(x_labels)), x_labels)
+plt.xlabel("Elapsed Time (Minutes)")
+plt.ylabel("Number of Photos")
+plt.title("Distribution of Photos Collected Over 1 Hour")
+plt.tight_layout()
+
+plt.savefig(os.getcwd() + "/hour_flickr.svg" , format='svg', dpi=1200)
 plt.show()
