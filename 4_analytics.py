fc59e3607b52b3776d76acbe83b4a4ab129dda13
Andrew Mackay <2192793m@student.gla.ac.uk>
2018-11-17 13:00:16 +0000

    finished basic counts, moved onto graphing

--- 4_analytics.py
+++ 4_analytics.py
@@ -13,8 +13,8 @@ db = client.twitterdb
 total_collected = db[collection].count()
 geo_tagged = db[collection].count_documents({"geo_is_public": 1})
 
-print("Total tweets: ", total_collected)
-print("Geo-tagged tweets: ", geo_tagged)
+print("Total Photos: ", total_collected)
+print("Geo-tagged Photos: ", geo_tagged)
 print("Geo-tagged: ", "{0:.2f}".format((geo_tagged/total_collected) * 100), "%\n")
 
 x = db[collection].aggregate([
@@ -35,10 +35,17 @@ for y in x:
     number_of_duplicates += y["count"]
 print("Number of Duplicates: ", number_of_duplicates, "\n")
 
-x = db[collection].find({}, {"views":1, "_id":0})
+x = db[collection].find({}, {"views":1, "_id":0, "id":1})
 total_views = 0
+max_views = 0
+max_id = None
 for picture in x:
-    total_views += int(picture["views"])
+    pic_views = int(picture["views"])
+    if pic_views > max_views:
+        max_views = pic_views
+        max_id = picture["id"]
+    total_views += pic_views
 
 print("Total photo views: ", total_views)
 print("Average views per photo: ", "{0:.4f}".format((total_collected/total_views)))
+print("Most views for an individual photo:", max_views, ". Flickr ID:", max_id, " URL: www.flickr.com/photo.gne?id="+max_id)
---
+++ 4_graphs.py
@@ -0,0 +1,277 @@
+import matplotlib.pyplot as plt
+from pymongo import MongoClient
+from datetime import datetime
+from datetime import timedelta
+import os
+
+
+def get_tweets_grouped_by_time(start_time, end_time, collection_name):
+    client = MongoClient()
+    db = client.twitterdb
+    collection = db[collection_name]
+    grouped_by_time = collection.aggregate([
+        {
+            "$match": {
+                "created_at": {
+                    "$gte": start_time,
+                    "$lt": end_time
+                }
+            }
+        },
+        {
+            '$group': {
+                "_id": { 
+                    "y": {'$year': '$created_at'},
+                    "m": {'$month': '$created_at'},
+                    "d": {'$dayOfMonth': '$created_at'},
+                    "h": {'$hour': '$created_at'},
+                    "interval": {
+                        "$subtract": [ 
+                        { "$minute": "$created_at" },
+                        { "$mod": [{ "$minute": "$created_at"}, 10] }
+                        ]
+                    }
+                },
+                "total": {'$sum': 1}
+            }
+        }
+    ])
+    return grouped_by_time
+
+def get_duplicates_grouped_by_time(start_time, end_time, collection_name):
+    client = MongoClient()
+    db = client.twitterdb
+    duplicate_ids = []
+    duplicates = db[collection_name].aggregate([
+        {            
+            "$match": {
+                "created_at": {
+                    "$gte": start_time,
+                    "$lt": end_time
+                }
+            }
+        },
+        {
+            "$group": {
+                "_id": {"id": "$id"},
+                "uniqueIds": {"$addToSet": "$_id"},
+                "count": {"$sum": 1} 
+            } 
+        }, 
+        {
+            "$match": { 
+                "count": {"$gte": 2 } 
+            }
+        }
+    ])
+    # get mongo ids for all duplicate tweets
+    for duplicate in duplicates:
+        for id in duplicate['uniqueIds']:
+            duplicate_ids.append(id)
+    grouped_by_time = db[collection_name].aggregate([
+        {
+            "$match": {
+                "_id": {
+                    "$in": duplicate_ids
+                    }
+            }
+        },
+        {
+            '$group': {
+                "_id": { 
+                    "y": {'$year': '$created_at'},
+                    "m": {'$month': '$created_at'},
+                    "d": {'$dayOfMonth': '$created_at'},
+                    "h": {'$hour': '$created_at'},
+                    "interval": {
+                        "$subtract": [ 
+                        { "$minute": "$created_at" },
+                        { "$mod": [{ "$minute": "$created_at"}, 10] }
+                        ]
+                    }
+                },
+                "total": {'$sum': 1}
+            }
+        }
+    ])
+    return grouped_by_time
+
+def get_retweeted_grouped_by_time(start_time, end_time, collection_name):
+    client = MongoClient()
+    db = client.twitterdb
+    collection = db[collection_name]
+    grouped_by_time = collection.aggregate([
+        {
+            "$match": {
+                "$and": [{
+                    "created_at": {
+                        "$gte": start_time,
+                        "$lt": end_time
+                    },
+                    "retweeted_status": {
+                        "$exists": True
+                    }
+                }]
+            }
+        },
+        {
+            '$group': {
+                "_id": { 
+                    "y": {'$year': '$created_at'},
+                    "m": {'$month': '$created_at'},
+                    "d": {'$dayOfMonth': '$created_at'},
+                    "h": {'$hour': '$created_at'},
+                    "interval": {
+                        "$subtract": [ 
+                        { "$minute": "$created_at" },
+                        { "$mod": [{ "$minute": "$created_at"}, 10] }
+                        ]
+                    }
+                },
+                "total": {'$sum': 1}
+            }
+        }
+    ])
+    return grouped_by_time
+
+def get_quoted_grouped_by_time(start_time, end_time, collection_name):
+    client = MongoClient()
+    db = client.twitterdb
+    collection = db[collection_name]
+    grouped_by_time = collection.aggregate([
+        {
+            "$match": {
+                "$and": [{
+                    "created_at": {
+                        "$gte": start_time,
+                        "$lt": end_time
+                    },
+                    "is_quote_status": True
+                }]
+            }
+        },
+        {
+            '$group': {
+                "_id": { 
+                    "y": {'$year': '$created_at'},
+                    "m": {'$month': '$created_at'},
+                    "d": {'$dayOfMonth': '$created_at'},
+                    "h": {'$hour': '$created_at'},
+                    "interval": {
+                        "$subtract": [ 
+                        { "$minute": "$created_at" },
+                        { "$mod": [{ "$minute": "$created_at"}, 10] }
+                        ]
+                    }
+                },
+                "total": {'$sum': 1}
+            }
+        }
+    ])
+    return grouped_by_time
+
+def plot_basic_histogram(start_time, end_time, collection_name, save=True):
+    grouped_tweets = get_tweets_grouped_by_time(start_time, end_time, collection_name)
+    heights_grouped = []
+    x_labels = range(0, 70, 10)
+    for group in grouped_tweets:
+        heights_grouped.append(group["total"])
+
+    plt.figure()
+    plt.bar(range(len(heights_grouped)), heights_grouped, align='edge')
+    plt.xticks(range(len(x_labels)), x_labels)
+    plt.xlabel("Elapsed Time (Minutes)")
+    plt.ylabel("Number of Tweets")
+    plt.title("Collection: " + collection_name)
+    if save:
+        plt.savefig(os.getcwd() + "/barcharts/" + collection_name + "_basic" + '.svg' , format='svg', dpi=1200)
+    plt.show()
+
+def plot_duplicate_histogram(start_time, end_time, collection_name, save=True):
+    grouped_tweets = get_tweets_grouped_by_time(start_time, end_time, collection_name)
+    duplicate_tweets = get_duplicates_grouped_by_time(start_time, end_time, collection_name)
+    heights_grouped = []
+    x_labels = range(0, 70, 10)
+    for group in grouped_tweets:
+        heights_grouped.append(group["total"])
+
+    heights_duplicates = []
+    for group in duplicate_tweets:
+        heights_duplicates.append(group["total"])
+
+    plt.figure()
+    plt.bar(range(len(heights_grouped)), heights_grouped, align='edge', label="Total Tweets")
+    plt.bar(range(len(heights_duplicates)), heights_duplicates, align='edge', label="Duplicates")
+    plt.xticks(range(len(x_labels)), x_labels)
+    plt.xlabel("Elapsed Time (Minutes)")
+    plt.ylabel("Number of Tweets")
+    plt.title("Collection: " + collection_name)
+    plt.legend()
+    if save:
+        plt.savefig(os.getcwd() + "/barcharts/" + collection_name + "_duplicates" + '.svg' , format='svg', dpi=1200)
+    plt.show()
+
+def plot_retweets_quotes_histogram(start_time, end_time, collection_name, save=True):
+    grouped_tweets = get_tweets_grouped_by_time(start_time, end_time, collection_name)
+    grouped_retweets = get_retweeted_grouped_by_time(start_time, end_time, collection_name)
+    grouped_quotes = get_quoted_grouped_by_time(start_time, end_time, collection_name)
+    heights_grouped = []
+    interval_tweets = []
+    interval_retweets = []
+    interval_quoted_tweets = []
+    x_labels = [x for x in range(0, 70, 10)]
+    for group in grouped_tweets:
+        heights_grouped.append(group["total"])
+        interval_tweets.append(group["_id"]["interval"]/10)
+
+    heights_retweets = []
+    for group in grouped_retweets:
+        heights_retweets.append(group["total"])
+        interval_retweets.append(group["_id"]["interval"]/10)
+
+    heights_quotes = []
+    for group in grouped_quotes:
+        heights_quotes.append(group["total"])
+        interval_quoted_tweets.append(group["_id"]["interval"]/10)
+
+
+    plt.figure()
+    if len(interval_tweets) > 0:
+        plt.bar(interval_tweets, heights_grouped, align='edge', label="Total Tweets")
+    if len(interval_retweets) > 0:
+        plt.bar(interval_retweets, heights_retweets, align='edge', label="Retweets")
+    if len(interval_quoted_tweets) > 0:
+        plt.bar(interval_quoted_tweets, heights_quotes, align='edge', label="Quotes")
+    plt.xticks(range(len(x_labels)), x_labels)
+    plt.xlabel("Duration of 10 Minutes")
+    plt.ylabel("Number of Tweets")
+    plt.title("Collection: " + collection_name)
+    plt.legend()
+    if save:
+        plt.savefig(os.getcwd() + "/barcharts/" + collection_name + "_rt_qt" + '.svg' , format='svg', dpi=1200)
+    plt.show()
+
+
+if __name__ == '__main__':
+    collections = {
+        "basic_crawler_1a": {
+            "start_time":datetime(2018, 11, 14, 11, 0),
+            "end_time":datetime(2018, 11, 14, 12, 0)
+            },
+        "enhanced_crawler_1b": {
+            "start_time":datetime(2018, 11, 14, 12, 0),
+            "end_time":datetime(2018, 11, 14, 13, 0)
+            },
+        "geo_tagged_1c": {
+            "start_time":datetime(2018, 11, 14, 13, 0),
+            "end_time":datetime(2018, 11, 14, 14, 0)
+            }
+        }
+    for collection, time in collections.items():
+        plot_basic_histogram(time["start_time"], time["end_time"], collection)
+        plot_duplicate_histogram(time["start_time"], time["end_time"], collection)
+        plot_retweets_quotes_histogram(time["start_time"], time["end_time"], collection)
+
+
+
+
